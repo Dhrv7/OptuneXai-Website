6ef956eb11a42d25b905c46f753a79227df47477
Dhruv Gupta <111233515+Dhrv7@users.noreply.github.com>
2025-07-13 01:01:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for Netlify static hosting
+  base: './', // << this is important for relative asset paths
 })
