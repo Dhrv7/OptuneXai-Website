48daa0f4ef273a13ffe4230a18ce913fe4e01fc5
Dhruv Gupta <111233515+Dhrv7@users.noreply.github.com>
2025-07-12 23:46:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Optional fix if you're absolutely sure it's necessary
-      external: ['/src/main.tsx'],
-    },
-  },
+  base: './', // Important for Netlify static hosting
 })
